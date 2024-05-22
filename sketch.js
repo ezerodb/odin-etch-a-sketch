1862edbc43521af39c45f7dfa2b45d8f735cd7b0
Ezequiel <ezequielr178@gmail.com>
2024-05-22 11:30:41 -0400

    Achieved both extra credit bonuses
    Squares get a random color and its opacity increases by 0.1 when mouseover

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let squares = document.querySelectorAll('.squares');
+    
 
     let canvas = document.querySelector("#grid");
 
@@ -20,24 +20,67 @@ document.addEventListener('DOMContentLoaded', function() {
         enter.addEventListener('click', () => {
             let newCanvasSize = parseInt(document.getElementById('size').value);
 
-            console.log(newCanvasSize);
+            if (newCanvasSize <= 100) {
 
-            let canvas = document.querySelector('.canvas');
-            canvas.innerHTML = "";
+                let canvas = document.querySelector('.canvas');
+                canvas.innerHTML = "";
 
-            for (let i = 0; i < newCanvasSize; i++) {
-                let row = document.createElement('div');
-                row.classList.add('row');
-                for (let l = 0; l < newCanvasSize; l++) {
-                    let square = document.createElement('div');
-                    let squareSize = 512 / newCanvasSize;
-                    square.setAttribute('style', `width:${squareSize}px; height:${squareSize}px`);
-                    square.classList.add('square');
-                    row.appendChild(square);
+                for (let i = 0; i < newCanvasSize; i++) {
+                    let row = document.createElement('div');
+                    row.classList.add('row');
+                    for (let l = 0; l < newCanvasSize; l++) {
+                        let square = document.createElement('div');
+                        let squareSize = 512 / newCanvasSize;
+                        square.setAttribute('style', `width:${squareSize}px; height:${squareSize}px`);
+                        square.classList.add('square');
+                        row.appendChild(square);
+                    }
+                    canvas.appendChild(row);
                 }
-                canvas.appendChild(row);
-            }
 
+                let squares = document.querySelectorAll('.square');
+                squares.forEach(square => {
+                    square.addEventListener('mouseover', () => {
+                        let element = window.getComputedStyle(square);
+                        let opacity = parseInt(element.getPropertyValue('opacity'));
+                        //console.log(element.getPropertyValue('background-color'));
+                        //console.log(opacity);
+                        if (element.getPropertyValue('background-color') === 'rgba(0, 0, 0, 0)') {
+                            square.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, 
+                                                                ${Math.floor(Math.random() * 255)}, 
+                                                                ${Math.floor(Math.random() * 255)})`;
+                            square.style.opacity = '0.1';
+                        } else if (parseFloat(element.getPropertyValue('opacity')) >= 0.1) {
+                            let opacity = parseFloat(element.getPropertyValue('opacity'));
+                            console.log(opacity);
+                            square.style.opacity = `${opacity + 0.1}`;
+                        }
+                    })
+                })
+
+            } else {
+                alert('Pick a smaller number');
+            }
+        })
+    })
+
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            let element = window.getComputedStyle(square);
+            
+            console.log(`Color is: ${element.getPropertyValue('background-color')}`);
+            
+            if (element.getPropertyValue('background-color') === 'rgba(0, 0, 0, 0)') {
+                square.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, 
+                                                    ${Math.floor(Math.random() * 255)}, 
+                                                    ${Math.floor(Math.random() * 255)})`;
+                square.style.opacity = '0.1';
+            } else if (parseFloat(element.getPropertyValue('opacity')) >= 0.1) {
+                let opacity = parseFloat(element.getPropertyValue('opacity'));
+                console.log(opacity);
+                square.style.opacity = `${opacity + 0.1}`;
+            }
         })
     })
 })
